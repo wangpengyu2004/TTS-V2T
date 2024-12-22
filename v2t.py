472bd83cc0a030bc760659a0f4eea2412dab88d3
wpy <121352810+wangpengyu2004@users.noreply.github.com>
2024-12-23 00:00:51 +0800

    Update v2t.py

--- v2t.py
+++ v2t.py
@@ -1,16 +1,23 @@
 import whisper
-def speech_to_text(file_path):
+import argparse
+def speech_to_text(args):
     # 加载 Whisper 模型
-    model = whisper.load_model("medium")  # 你可以选择其他模型，如 "small", "medium", "large"
-    
+    model = whisper.load_model(args.size)  # 你可以选择其他模型，如 "small", "medium", "large"
     # 加载音频文件
-    audio = whisper.load_audio(file_path)
+    audio = whisper.load_audio(args.file_path)
     audio = whisper.pad_or_trim(audio)
     # 转换语音为文本
     result = model.transcribe(audio)
     # 打印识别结果
     print(f"result: {result['text']}")
+    with open(args.output_path,"w") as f:
+        f.write(result)
+    
 
 if __name__ == "__main__":
-    file_path = "/home/wpy/wpy_workspace/s2s/VITS/raw_audio/wpy_1.wav"  # 替换成你音频文件的路径
-    speech_to_text(file_path)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--file_path", default="./file_path.wav", help="your file path")
+    parser.add_argument("--size", default="small", help=" your model size")
+    parser.add_argument("--output_path", default="./output_path.txt", help="your file path")
+    args = parser.parse_args()
+    speech_to_text(args)
